95945dfc70db1c331446b4184a9613ecb917c1a3
Shakir21018545 <21018545@myrp.edu.sg>
2023-06-01 15:21:24 +0800

    completed

--- app/src/main/java/sg/edu/rp/c346/id21018545/wk7demospinnertest/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018545/wk7demospinnertest/MainActivity.java
@@ -3,12 +3,41 @@ package sg.edu.rp.c346.id21018545.wk7demospinnertest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+        spinner = findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                switch(position){
+                    case 0:
+                        tv.setText("Spinner item, YES Selected");
+                        break;
+                    case 1:
+                        tv.setText("Spinner item, NO Selected");
+                        break;
+
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+
+            }
+        });
     }
 }
